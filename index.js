83358e6c15e9acc861c670870edf5d1890f76e43
Daniel Griesser <daniel.griesser.86@gmail.com>
2022-12-05 10:37:26 +0100

    feat: Add project sync for ingest team

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ dotenv.config();
 
 import syncFrontend from "./frontend/index.js";
 import syncBackend from "./backend/index.js";
+import syncIngest from "./ingest/index.js";
 
 syncFrontend();
 syncBackend();
+syncIngest();
