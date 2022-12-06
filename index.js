b8ca87bf81256cd4e9873514170c54abaac705a8
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2022-12-06 10:19:27 +0100

    feat: add Sentry

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import Sentry from "@sentry/node";
 import syncFrontend from "./frontend/index.js";
 import syncBackend from "./backend/index.js";
 import syncIngest from "./ingest/index.js";
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+});
 
 syncFrontend();
 syncBackend();
