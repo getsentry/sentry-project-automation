a0d22f9d51115952ebf841bf2a7536733a9db3f5
jernejstrasner <jernejstrasner@users.noreply.github.com>
2023-03-14 17:03:13 +0100

    Added Sentry Performance

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import Sentry from "@sentry/node";
 import syncFrontend from "./frontend/index.js";
 import syncBackend from "./backend/index.js";
 import syncReplay from "./replay/index.js";
 import syncIngest from "./ingest/index.js";
 
+import Sentry from "@sentry/node";
+import "@sentry/tracing";
+import { ProfilingIntegration } from "@sentry/profiling-node";
+
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new ProfilingIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0,
 });
 
+const frontendTransaction = Sentry.startTransaction({name: "Sync Frontend"});
 syncFrontend();
+frontendTransaction.finish();
+const backendTransaction = Sentry.startTransaction({name: "Sync Backend"});
 syncBackend();
+backendTransaction.finish();
+const replayTransaction = Sentry.startTransaction({name: "Sync Replay"});
 syncReplay();
+replayTransaction.finish();
+const ingestTransaction = Sentry.startTransaction({name: "Sync Ingest"});
 syncIngest();
+ingestTransaction.finish();
